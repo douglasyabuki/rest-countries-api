677ccd17960ddc8a8e03263c8076aab78fd01696
douglasyabuki <douglasyabuki@gmail.com>
2024-01-04 18:01:41 -0300

    Add: Flag Icon

--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -64,3 +64,32 @@ export const ArrowBackIcon = () => {
     </svg>
   );
 };
+
+export const FlagIcon = () => {
+  return (
+    <svg
+      width="64px"
+      height="64px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M5 21V3.90002C5 3.90002 5.875 3 8.5 3C11.125 3 12.875 4.8 15.5 4.8C18.125 4.8 19 3.9 19 3.9V14.7C19 14.7 18.125 15.6 15.5 15.6C12.875 15.6 11.125 13.8 8.5 13.8C5.875 13.8 5 14.7 5 14.7"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="stroke-light-mode-text transition-colors duration-150 dark:stroke-dark-mode-text"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+};

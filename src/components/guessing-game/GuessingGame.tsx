d7efa48ac6df4bdbb0644a510f2d6acc26e855f4
douglasyabuki <douglasyabuki@gmail.com>
2024-01-04 20:11:54 -0300

    Mock: Score animation

--- src/components/guessing-game/GuessingGame.tsx
+++ src/components/guessing-game/GuessingGame.tsx
@@ -11,9 +11,34 @@ export interface IGuessingGameProps {
 export default function GuessingGame({ allCountries }: IGuessingGameProps) {
   const [count, setCount] = useState({ rights: 0, wrongs: 0 });
 
+  const addWrongAnswer = () => {
+    setCount((prev) => {
+      return { ...prev, wrongs: prev.wrongs + 1 };
+    });
+  };
+  const addRightAnswer = () => {
+    setCount((prev) => {
+      return { ...prev, rights: prev.rights + 1 };
+    });
+  };
+
   return (
     <div className="flex h-full w-screen flex-col items-center gap-4 overflow-y-auto overflow-x-hidden">
       <Scoreboard count={count}></Scoreboard>
+      <div className="flex w-[250px] items-center justify-between">
+        <button
+          onClick={addRightAnswer}
+          className="w-[100px] rounded-md border-[1px] border-dark-mode-element px-4 py-3 font-bold"
+        >
+          RIGHT!
+        </button>
+        <button
+          onClick={addWrongAnswer}
+          className="w-[100px] rounded-md border-[1px] border-dark-mode-element px-4 py-3 font-bold"
+        >
+          WRONG!
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/guessing-game/scoreboard/Scoreboard.tsx
+++ src/components/guessing-game/scoreboard/Scoreboard.tsx
@@ -1,25 +1,76 @@
 "use client";
 import { FlagIcon } from "@/components/icons/Icons";
-import * as React from "react";
-
+import { useEffect, useState } from "react";
 interface ScoreboardProps {
   count: { rights: number; wrongs: number };
 }
 
 export default function Scoreboard({ count }: ScoreboardProps) {
+  const [blockPosition, setBlockPosition] = useState<string | null>(null);
+  const [displayedCount, setDisplayedCount] = useState({
+    rights: 0,
+    wrongs: 0,
+  });
+
+  useEffect(() => {
+    let timeout: ReturnType<typeof setTimeout>;
+
+    if (count.rights !== displayedCount.rights) {
+      setBlockPosition("start");
+      timeout = setTimeout(() => {
+        setBlockPosition(null);
+        setDisplayedCount((prev) => {
+          return { ...prev, rights: count.rights };
+        });
+      }, 500);
+    }
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [count.rights, displayedCount.rights]);
+
+  useEffect(() => {
+    let timeout: ReturnType<typeof setTimeout>;
+
+    if (count.wrongs !== displayedCount.wrongs) {
+      setBlockPosition("end");
+      timeout = setTimeout(() => {
+        setBlockPosition(null);
+        setDisplayedCount((prev) => {
+          return { ...prev, wrongs: count.wrongs };
+        });
+      }, 500);
+    }
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [count.wrongs, displayedCount.wrongs]);
+
   return (
     <div className="element flex h-[150px] w-[300px] flex-col items-center justify-start gap-4 rounded-md px-6 py-3 shadow-lg shadow-transparent-black">
       <h1>Scoreboard</h1>
-      <div className="flex h-[80px] w-[240px] items-center justify-between">
+      <div className="flex h-[80px] w-[260px] items-center justify-between">
         <div className="flex h-auto w-[80px] flex-col items-center justify-center gap-4">
           <h3>RIGHTS</h3>
-          <h2 className="text-3xl">{count.rights}</h2>
+          <h2 className="text-3xl">{displayedCount.rights}</h2>
         </div>
         <div className="flex h-auto w-[80px] flex-col items-center justify-center gap-4">
           <h3>WRONGS</h3>
-          <h2 className="text-3xl">{count.wrongs}</h2>
+          <h2 className="text-3xl">{displayedCount.wrongs}</h2>
         </div>
-        <div className="absolute flex h-[80px] w-[80px] translate-x-20 transform-gpu items-center justify-center rounded-md bg-light-mode-background p-4 shadow-lg shadow-transparent-black transition-transform duration-150 dark:bg-dark-mode-background">
+        <div
+          className={`${
+            blockPosition === null
+              ? `translate-x-[90px]`
+              : `${
+                  blockPosition === "start"
+                    ? `translate-x-0`
+                    : `translate-x-[180px]`
+                }`
+          } absolute flex h-[80px] w-[80px] transform-gpu items-center justify-center rounded-md bg-light-mode-background p-4 shadow-lg shadow-transparent-black transition-transform duration-150 dark:bg-dark-mode-background`}
+        >
           <FlagIcon />
         </div>
       </div>

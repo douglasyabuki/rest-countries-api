9a709a18043b7c12f2b688cea5c9a3b6e185d12c
douglasyabuki <douglasyabuki@gmail.com>
2024-01-08 16:11:23 -0300

    Update: Minor layout changes

--- src/components/filterable-countries/filtered-countries/country-card/fake-card/FakeCard.tsx
+++ src/components/filterable-countries/filtered-countries/country-card/fake-card/FakeCard.tsx
@@ -9,12 +9,12 @@ export default function FakeCard() {
       className="element flex h-[400px] w-[300px] transform-gpu animate-pulse flex-col items-center justify-start overflow-hidden rounded-md shadow-sm shadow-transparent-black hover:-translate-y-2 hover:shadow-md"
       href={"/game"}
     >
-      <div className="h-[200px] w-[300px] bg-dark-mode-element"></div>
+      <div className="h-[200px] w-[300px] bg-dark-mode-element dark:bg-light-mode-element"></div>
       <div className="flex w-full flex-col items-start justify-start gap-2 p-4">
-        <div className="h-9 w-full rounded-md bg-dark-mode-element py-1"></div>
-        <div className="h-6 w-2/3 rounded-md bg-dark-mode-element"></div>
-        <div className="h-6 w-2/3 rounded-md bg-dark-mode-element"></div>
-        <div className="h-6 w-2/3 rounded-md bg-dark-mode-element"></div>
+        <div className="h-9 w-full rounded-md bg-dark-mode-element py-1 dark:bg-light-mode-element"></div>
+        <div className="h-6 w-2/3 rounded-md bg-dark-mode-element dark:bg-light-mode-element"></div>
+        <div className="h-6 w-2/3 rounded-md bg-dark-mode-element dark:bg-light-mode-element"></div>
+        <div className="h-6 w-2/3 rounded-md bg-dark-mode-element dark:bg-light-mode-element"></div>
       </div>
     </Link>
   );

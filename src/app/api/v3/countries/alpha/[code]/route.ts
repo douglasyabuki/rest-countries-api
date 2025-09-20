7020271f209f06b0490590a81c6715f402ff851f
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-09-20 12:55:24 -0300

    Fix: Update country data retrieval to include cca3 code in GET request

--- src/app/api/v3/countries/alpha/[code]/route.ts
+++ src/app/api/v3/countries/alpha/[code]/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server";
 import data from "../../data.json";
 
-export async function GET(request: Request, context: any) {
+export async function GET(_: Request, context: any) {
   const { params } = context;
-  const country = data.filter(({ ccn3, cioc, cca2 }) =>
-    [ccn3, cioc, cca2].includes(params.code),
+  const country = data.find(({ ccn3, cioc, cca2, cca3 }) =>
+    [ccn3, cioc, cca2, cca3].includes(params.code),
   );
   return NextResponse.json({
     country,
--- src/app/api/v3/countries/codes/route.ts
+++ src/app/api/v3/countries/codes/route.ts
@@ -3,8 +3,8 @@ import data from "../data.json";
 
 export async function GET() {
   const codes = data.map((item) => {
-    const { cca2, ccn3, cioc, ...rest } = item;
-    return { cca2, ccn3, cioc };
+    const { cca2, cca3, ccn3, cioc } = item;
+    return { cca2, cca3, ccn3, cioc };
   });
   return NextResponse.json({
     codes,

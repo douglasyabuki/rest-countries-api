1f5012c6578ede83f5b447fd0f2fedf01a6e38e7
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-09-20 16:20:04 -0300

    Refactor: Enhance GET endpoint to filter data based on query parameters

--- src/app/api/iso-693-3/route.ts
+++ src/app/api/iso-693-3/route.ts
@@ -1,8 +1,17 @@
+import { getQueryParams } from "@/utils/request-utils";
 import { NextResponse } from "next/server";
 import data from "./iso-693-3.json";
 
-export async function GET() {
+export async function GET(request: Request) {
+  const { ids } = getQueryParams(request.url);
+  const filters: string[] = [];
+  if (ids?.length > 0) {
+    ids.split(",").forEach((i) => filters.push(i.toLowerCase()));
+  }
   return NextResponse.json({
-    data,
+    data:
+      filters.length > 0
+        ? data.filter((row) => filters.includes(row.id))
+        : data,
   });
 }

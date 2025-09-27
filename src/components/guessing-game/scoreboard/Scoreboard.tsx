17adfadd2d9c253154e144daba5ccd4fa8f935d8
Douglas Yuji Yabuki <douglasyabuki@gmail.com>
2025-09-27 11:13:26 -0300

    Update: Minor layout changes

--- src/components/guessing-game/scoreboard/Scoreboard.tsx
+++ src/components/guessing-game/scoreboard/Scoreboard.tsx
@@ -60,12 +60,10 @@ export function Scoreboard({ gameStage, onReset }: Scoreboard) {
         <div className="relative flex w-full items-center justify-center">
           <button
             className={twMerge(
-              "absolute left-0 h-6 w-6 items-center justify-center rounded-full bg-light-mode-background p-1 duration-150 focus:outline-none disabled:opacity-60 dark:bg-dark-mode-background",
-              gameStage === gameStages.RESETTING ? "hidden" : "flex",
+              "absolute left-0 flex h-6 w-6 items-center justify-center rounded-full bg-light-mode-background p-1 duration-150 focus:outline-none disabled:opacity-60 dark:bg-dark-mode-background",
               isSettingsToggled && "rotate-180",
             )}
             onClick={() => setIsSettingsToggled(!isSettingsToggled)}
-            disabled={isReloadButtonDisabled}
           >
             <SettingsIcon />
           </button>
